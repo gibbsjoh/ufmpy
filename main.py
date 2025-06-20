96043187a703bf5de22a6b9d5894b9553b4a717a
gibbsjoh <gibbsjoh@gmail.com>
2025-06-20 11:06:53 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,70 @@
-# Example / testing for ufmpy
-# Micropython calls boot.py then main.py
-# This is an example main.py to call the functions in ufmpy (for testing as well as a guide)
+# MicroPython script to get data from FileMaker Server via data api and display on I2C LCD
 
-# import the file. it must be in the same directory as main.py, but you could change that
 import ufmpy
+import ujson
+lcd.clear()
 
-# get a token for the specified FM solution
-token = ufmpy.fmGetToken("MyFMSolution")
-print(token)
+## demo to get server info, then query a database
+whatToDo = "solution"
+
+if(whatToDo == "solution"):
+    lcd.putstr("Getting FMS Token")
+    token = ufmpy.fmGetToken("HomeList")
+
+    if (token[:5] != "Error"):
+        #do other stuff here
+        print("success!")
+        haveToken = 1
+        sleep(2)
+        lcd.clear()
+        lcd.putstr("Got token!")
+    else:
+        haveToken = 0
+        print(token)
+
+    if (haveToken == 1):
+        #query database
+        theQuery = {
+            "query": [
+                {"AppName": "Netflix"},
+            ],
+            "sort": [
+                {"fieldName": "AppName", "sortOrder": "ascend"},
+            ]
+        }
+        theResult = ufmpy.fmPerformFind("HomeList", "dataapi_APPS", theQuery, token)
+        print(theResult)
+        # get the record ID
+        theRecordID = theResult["response"]["data"][0]["recordId"]
+        print(theRecordID)
+        sleep(3)
+        # edit the record
+        thePayload = {"fieldData":{"zz_Dummy1":"I love MicroPython"}}
+        theResult = ufmpy.editRecord("HomeList", "dataapi_APPS", theRecordID, thePayload, token)
+        print(theResult)
+    
+    # edit a record
+
+elif(whatToDo == "admin"):
+    lcd.putstr("Getting token..")
+    adminToken = ufmpy.fmGetAdminToken()
+    sleep(3)
+    lcd.move_to(0,1)
+    lcd.putstr("Got Token!")
+    #print("Get token result:")
+    #print(adminToken)
+    # get the server name and version
+    sleep(3)
+    lcd.clear()
+    metaData = ufmpy.fmGetServerInfo(adminToken)
+    theName = metaData["response"]["ServerName"]
+    theVersion = metaData["response"]["ServerVersion"]
+    lcd.putstr(theName)
+    lcd.move_to(0,1)
+    lcd.putstr(theVersion)
+    sleep(5)
+    #print(theName, " ", theVersion)
+    # kill the token
+    killToken = ufmpy.fmInvalidateAdminToken(adminToken)
+    print("Invalidate Token result: ", ujson.dumps(killToken))
+
--- ufmpy.py
+++ ufmpy.py
@@ -3,18 +3,28 @@
 import urequests
 import ubinascii
 import json
+import ujson
+import utime
+import re
 
 # set server name, creds, API endpoints
 # for the moment, the server is defind here
 # tbd in future version - pass the server etc as parameters
-fmServer = "https://fmserver.example.com"
-accountName = "dataapi"
-password = "password"
+fmServer = "https://myfmserver"
+accountName = "user"
+password = "0ccur4nc3!"
+
+adminAccount = "admin"
+adminPassword = "password"
 
 # the username/password is passed as username:password, Base64 encoded
 creds = accountName + ":" + password
 credsEncoded = ubinascii.b2a_base64(creds).strip()
-credsEncoded = credsEnc.decode('utf-8')
+credsEncoded = credsEncoded.decode('utf-8')
+
+adminCreds = adminAccount + ":" + adminPassword
+adminCredsEncoded = ubinascii.b2a_base64(adminCreds)
+adminCredsEncoded = adminCredsEncoded.decode('utf-8').strip()
 
 # solution name and api version
 solutionName = "HomeList"
@@ -24,10 +34,101 @@ apiVersion = "v1"
 
 # define functions
 ####################################
+# get Admin API Token              #
+####################################
+def fmGetAdminToken():
+    theEndpoint = "/fmi/admin/api/v2/user/auth"
+    authString ="Basic " + str(adminCredsEncoded)
+    #print (authString)
+    # FMS seems to need an empty data payload and a Content-Length header, so we do that here:
+    data = json.dumps({}).encode('utf-8')
+    contentLength = len(data)
+
+    # define the URL
+    url = fmServer + theEndpoint
+
+    # set the headers
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": authString,
+        "Content-Length": "0"
+    }
+    # Properly format headers as a JSON string
+    headers_json = ujson.dumps(headers)
+
+    # # query the enpoint
+    response = urequests.post(url, headers=ujson.loads(headers_json))
+
+    # Check the response
+    if response.status_code == 200:
+        theResponse = response.json()
+        theToken = theResponse["response"]["token"]
+        return str(theToken)
+    else:
+        return ("Error ", response.status_code)
+    # END of Function
+
+######################################
+# Get the server info from Admin API #
+######################################
+def fmGetServerInfo(theToken):
+    # returns server metadata:
+    # "serverName": "localhost",
+    #"serverID": "3EAE027BD85CFF61ACB745A7B04D6D8AEA5263EE8604C7E8CFBD1B3BDACE7F26",
+    #"serverIP": "192.168.1.1",
+    #"serverVersion": "19.0.1.103",
+    #"serverHostTime": "2023-01-01T08:05:01"
+    # define the endpoint
+    theEndpoint = "/fmi/admin/api/v2/server/metadata"
+    url = fmServer + theEndpoint
+
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + theToken,
+        "Content-Length": "0"
+    }
+    # Properly format headers as a JSON string
+    headers_json = ujson.dumps(headers)
+
+    response = urequests.get(url, headers=ujson.loads(headers_json))
+
+    # Check and return the response
+    if response.status_code == 200:
+        theResponse = response.json()
+        return theResponse
+    else:
+        return response.status_code
+
+##########################################################
+# Invalidate the token (so we don't run into api limits) #
+#########################################################
+def fmInvalidateAdminToken(theToken):
+    theEndpoint = "/fmi/admin/api/v2/user/auth/" + theToken
+    url = fmServer + theEndpoint
+
+    headers = {
+        "Content-Type": "application/json",
+        "Content-Length": "0"
+    }
+    # Properly format headers as a JSON string
+    headers_json = ujson.dumps(headers)
+
+    response = urequests.delete(url, headers=ujson.loads(headers_json))
+
+    # Check and return the response
+    if response.status_code == 200:
+        theResponse = response.json()
+        return theResponse
+    else:
+        return response.status_code
+
+
+####################################
 # get token for specified solution #
 ####################################
 def fmGetToken(solutionName):
     theEndpoint = "/fmi/data/" + apiVersion + "/databases/" + solutionName + "/sessions"
+    authString = "Basic " + str(credsEncoded)
     # FMS seems to need an empty data payload and a Content-Length header, so we do that here:
     data = json.dumps({}).encode('utf-8')
     contentLength = len(data)
@@ -71,7 +172,7 @@ def currentTimestamp():
 def fmPerformFind(theSolution, theLayout, theQuery, theToken):
     # lots of params here, all self explanatory. needs the token we got from the function above
     # define the endpoint
-    theEndpoint = "/fmi/data/" + apiVersion + "/databases/" + solutionName + "/layouts/" + theLayout + "/_find"
+    theEndpoint = "/fmi/data/" + apiVersion + "/databases/" + theSolution + "/layouts/" + theLayout + "/_find"
     url = fmServer + theEndpoint
 
     # example query with sort, see https://help.claris.com/en/data-api-guide/content/perform-find-request.html
@@ -93,6 +194,33 @@ def fmPerformFind(theSolution, theLayout, theQuery, theToken):
     }
 
     response = urequests.post(url, headers=headers, data=data)
+
+    # Check and return the response
+    if response.status_code == 200:
+        theResponse = response.json()
+        return theResponse
+    else:
+        return response.status_code
+
+########################################
+# modify a specific record by recordid #
+########################################
+def editRecord(theSolution, theLayout, theRecordID, thePayload, theToken):
+    # lots of params here, all self explanatory. needs the token we got from the function above
+    # define the endpoint
+    theEndpoint = "/fmi/data/" + apiVersion + "/databases/" + theSolution + "/layouts/" + theLayout + "/records/" + theRecordID
+    url = fmServer + theEndpoint
+
+    data = json.dumps(thePayload).encode('utf-8')
+    contentLength = len(data)
+
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + theToken,
+        "Content-Length": str(contentLength)
+    }
+
+    response = urequests.patch(url, headers=headers, data=data)
 
     # Check and return the response
     if response.status_code == 200:
